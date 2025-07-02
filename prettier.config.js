ea06082600965318302d2d635b9927df4330c475
Rihanneh <201752109+Rihanneh@users.noreply.github.com>
2025-07-02 09:44:58 +0200

    config: .prettierignore et prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  semi: true, // Met un point-virgule à la fin de chaque instruction
+  trailingComma: "none", // N’ajoute PAS de virgule après le dernier élément (ex: tableaux/objets)
+  singleQuote: true, // Utilise des apostrophes (simples quotes : ') au lieu des guillemets doubles (")
+  printWidth: 100, // Limite la longueur d’une ligne à 100 caractères max
+  tabWidth: 2, // Utilise 2 espaces pour chaque niveau d’indentation
+  useTabs: false, // Indente avec des espaces (et non des tabulations)
+};
