bbfc03b3fc1299ef9c99b5f74c4984df4b5abcce
Rihanneh <201752109+Rihanneh@users.noreply.github.com>
2025-07-07 18:00:00 +0200

    config: test config server

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 
-// Middleware Json
+// Middleware JSON
 app.use(express.json());
 
-//Route de santé (health check)
-app.get("/health", (_req: Request, res: Response): void => {
+// Route de santé (health check)
+app.get('/health', (_req: Request, res: Response): void => {
   res.status(200).json({
-    status: "ok",
-    message: "API is running",
+    status: 'OK',
+    message: 'API is running'
   });
 });
 
 // Route par défaut
 app.get('/', (_req: Request, res: Response): void => {
-res.send(`
-    <h1> Bookstore API </h1>
-    <p><a href="/health"> Health Check </a></p>`);
+  res.send(`
+        <h1> Bookstore API </h1>
+        <p><a href="/health"> Health Check </a></p>`);
 });
 
 export default app;
