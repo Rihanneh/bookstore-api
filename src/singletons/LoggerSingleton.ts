9f2a91ebe5a66bfd8a0656114757e66a5f563291
Rihanneh <201752109+Rihanneh@users.noreply.github.com>
2025-07-16 10:18:52 +0200

    feat: add loggerSingleton and TDD green Ok

---
+++ src/singletons/LoggerSingleton.ts
@@ -0,0 +1,63 @@
+import winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+
+/**
+ * Singleton pour winston logger
+ */
+
+export class LoggerSingleton {
+  // Propriété (private, protected, public)
+  static #instance: winston.Logger;
+
+  private constructor() {
+    // Empêche l'instanciation directe
+  }
+
+  /**
+   * Returns the singleton instance of the winston logger.
+   *
+   * This method ensures that only one instance of the logger is created
+   * throughout the application. The logger is configured with specified
+   * log levels, formats, and transports, including console and
+   * daily rotating file transports for errors and audits.
+   *
+   * @returns {winston.Logger} The singleton logger instance.
+   */
+
+  public static getInstance(): winston.Logger {
+    if (!LoggerSingleton.#instance) {
+      LoggerSingleton.#instance = winston.createLogger({
+        level: process.env.LOG_LEVEL ?? 'info',
+        format: winston.format.combine(
+          winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
+          winston.format.errors({ stack: true }),
+          winston.format.json(),
+          winston.format.colorize()
+        ),
+        transports: [
+          new winston.transports.Console(),
+
+          // ERREURS (30 jours, max 10MB par fichier)
+          new DailyRotateFile({
+            filename: 'logs/error-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            level: 'error',
+            maxSize: '10m',
+            zippedArchive: true,
+            maxFiles: '30d'
+          }),
+          // AUDIT RGPD (1 an, max 20MB par fichier)
+          new DailyRotateFile({
+            filename: 'logs/audit-%DATE%.log',
+            datePattern: 'YYYY-MM-DD',
+            level: 'info',
+            maxSize: '20m',
+            zippedArchive: true,
+            maxFiles: '365d'
+          })
+        ]
+      });
+    }
+    return LoggerSingleton.#instance;
+  }
+}
