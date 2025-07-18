dc4a3fe183cb2c6caf01105b2686bc86f2ca5f60
Rihanneh <201752109+Rihanneh@users.noreply.github.com>
2025-07-18 22:58:39 +0200

    fix: Jest test path with @ annotation

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,20 @@
 /** @type {import('jest').Config} */
 export default {
-  preset: "ts-jest/presets/default-esm",
-  extensionsToTreatAsEsm: [".ts"],
-  testEnvironment: "node",
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
 
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@/(.*)$': '<rootDir>/src/$1'
   },
 
   transform: {
-    "^.+\\.ts$": ["ts-jest", { useESM: true }],
+    '^.+\\.ts$': ['ts-jest', { useESM: true }]
   },
 
-  testMatch: ["**/__tests__/**/*.test.ts"],
+  testMatch: ['**/__tests__/**/*.test.ts'],
 
   clearMocks: true,
-  verbose: true,
+  verbose: true
 };
