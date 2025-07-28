354250ae35c4d086e38f97cb30c14c3de164035d
Rihanneh <201752109+Rihanneh@users.noreply.github.com>
2025-07-28 09:57:34 +0200

    feat: Implement code for BlacklistService and Vitest TDD GREEN

---
+++ src/services/BlacklistService.ts
@@ -0,0 +1,21 @@
+import { IBlacklistService } from '@/interfaces/security/IBlacklistService';
+
+export class BlacklistService implements IBlacklistService {
+  static #instance: BlacklistService;
+  private readonly blacklistedTokens: Set<string> = new Set<string>();
+
+  private constructor() {}
+
+  public static getInstance(): BlacklistService {
+    if (!BlacklistService.#instance) {
+      BlacklistService.#instance = new BlacklistService();
+    }
+    return BlacklistService.#instance;
+  }
+  public async addToken(jti: string): Promise<void> {
+    this.blacklistedTokens.add(jti);
+  }
+  public async isTokenBlacklisted(jti: string): Promise<boolean> {
+    return this.blacklistedTokens.has(jti);
+  }
+}
